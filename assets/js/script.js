c597c18fb412755a5a51ba1d283c631087cc3af0
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-30 23:21:03 -0600

    grabbing city function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-const citySearch = $("city-search");
-const searchBtn = $("search-button");
-const clearBtn = $("clear-search");
-const historyEl = $("search-history");
-const todayWeatherEl = $("weather-today");
-const cityNameEl = $("city-search");
-const weatherPicEl = $("weather-icon");
-const currentWindEl = $("today-wind-speed");
-const currentHumidityEl = $("today-humidity");
-const forecastEl = $("forecast");
+const citySearch = document.getElementById("city-search");
+const searchBtn = document.getElementById("search-button");
+const clearBtn = document.getElementById("clear-search");
+const historyEl = document.getElementById("search-history");
+const todayWeatherEl = document.getElementById("weather-today");
+const weatherPicEl = document.getElementById("weather-icon");
+const currentWindEl = document.getElementById("today-wind-speed");
+const currentHumidityEl = document.getElementById("today-humidity");
+const forecastEl = document.getElementById("forecast");
 
 
 // Saving API key variable
-var APIKey = a1fe84e1ab8a4fe1c1aa03d84e6c4570;
+var apiKey = "a1fe84e1ab8a4fe1c1aa03d84e6c4570";
 
 var city;
 
+searchBtn.addEventListener("click", grabCity)
 
+function grabCity(event) {
+    event.preventDefault();
+
+    var cityName = $("#city-search").val();
+    getCityWeather(cityName);
+
+    // var cityName = $("city-search").val()
+    // console.log(cityName)
+}
