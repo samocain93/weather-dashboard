19a5dc81745eab17138bc129045562e015f1b6a8
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-30 22:43:32 -0600

    create doc variables

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const citySearch = $("city-search");
+const searchBtn = $("search-button");
+const clearBtn = $("clear-search");
+const historyEl = $("search-history");
+const todayWeatherEl = $("weather-today");
+const cityNameEl = $("city-search");
+const weatherPicEl = $("weather-icon");
+const currentWindEl = $("today-wind-speed");
+const currentHumidityEl = $("today-humidity");
+const forecastEl = $("forecast");
+
+
+// Saving API key variable
+var APIKey = a1fe84e1ab8a4fe1c1aa03d84e6c4570;
+
+var city;
+
+
